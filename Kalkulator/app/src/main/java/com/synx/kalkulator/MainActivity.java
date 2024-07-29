9cffddc99ffd61f606653ac29c5e2b359b090958
SafiaRashidaRaya <140325055+SafiaRashidaRaya@users.noreply.github.com>
2024-07-29 14:17:58 +0700

    versi 1.6

--- Kalkulator/app/src/main/java/com/synx/kalkulator/MainActivity.java
+++ Kalkulator/app/src/main/java/com/synx/kalkulator/MainActivity.java
@@ -1,6 +1,10 @@
 package com.synx.kalkulator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvHasil;
+    EditText etBil_1, etBil_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,67 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        
+
+        load();
+
+    }
+
+    public void load(){
+        tvHasil = findViewById(R.id.tvHasil);
+        etBil_1 = findViewById(R.id.etBil_1);
+        etBil_2 = findViewById(R.id.etBil_2);
+    }
+
+    public void btnJumlah(View view) {
+        if (etBil_1.getText().toString().equals("") || etBil_2.getText().toString().equals("")){
+            Toast.makeText(this, "Ada bilangan yang masih kosong", Toast.LENGTH_SHORT).show();
+        }else {
+            double bil_1 = Double.parseDouble(etBil_1.getText().toString());
+            double bil_2 = Double.parseDouble(etBil_2.getText().toString());
+
+            double hasil = bil_1 + bil_2;
+
+            tvHasil.setText(hasil+"");
+        }
+
+    }
+
+    public void btnKurang(View view) {
+        if (etBil_1.getText().toString().equals("") || etBil_2.getText().toString().equals("")){
+            Toast.makeText(this, "Ada bilangan yang masih kosong", Toast.LENGTH_SHORT).show();
+        }else {
+            double bil_1 = Double.parseDouble(etBil_1.getText().toString());
+            double bil_2 = Double.parseDouble(etBil_2.getText().toString());
+
+            double hasil = bil_1 - bil_2;
+
+            tvHasil.setText(hasil+"");
+        }
+    }
+
+    public void btnKali(View view) {
+        if (etBil_1.getText().toString().equals("") || etBil_2.getText().toString().equals("")){
+            Toast.makeText(this, "Ada bilangan yang masih kosong", Toast.LENGTH_SHORT).show();
+        }else {
+            double bil_1 = Double.parseDouble(etBil_1.getText().toString());
+            double bil_2 = Double.parseDouble(etBil_2.getText().toString());
+
+            double hasil = bil_1 * bil_2;
+
+            tvHasil.setText(hasil+"");
+        }
+    }
+
+    public void btnBagi(View view) {
+        if (etBil_1.getText().toString().equals("") || etBil_2.getText().toString().equals("")){
+            Toast.makeText(this, "Ada bilangan yang masih kosong", Toast.LENGTH_SHORT).show();
+        }else {
+            double bil_1 = Double.parseDouble(etBil_1.getText().toString());
+            double bil_2 = Double.parseDouble(etBil_2.getText().toString());
+
+            double hasil = bil_1 / bil_2;
+
+            tvHasil.setText(hasil+"");
+        }
     }
 }
