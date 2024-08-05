d9118a63318f2b0f38acddf6f58e60c8bcc8c7bf
SafiaRashidaRaya <140325055+SafiaRashidaRaya@users.noreply.github.com>
2024-08-05 22:44:15 +0700

    2.9

--- RecyclerViewCardView/app/src/main/java/com/synx/recyclerviewcardview/MainActivity.java
+++ RecyclerViewCardView/app/src/main/java/com/synx/recyclerviewcardview/MainActivity.java
@@ -7,8 +7,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    SiswaAdapter adapter;
+    List<Siswa> siswaList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,37 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        load();
+        isiData();
+    }
+
+    public void load(){
+        recyclerView = findViewById(R.id.rcvSiswa);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    public void isiData(){
+        siswaList = new ArrayList<Siswa>();
+        siswaList.add(new Siswa("Seresya", "Aeniur"));
+        siswaList.add(new Siswa("Ayseres", "Aeniur"));
+        siswaList.add(new Siswa("Haronel", "Aeniur"));
+        siswaList.add(new Siswa("Arenvin", "Aeniur"));
+        siswaList.add(new Siswa("Lawrence", "Aeniur"));
+        siswaList.add(new Siswa("Icerta", "Aeniur"));
+        siswaList.add(new Siswa("Riferell", "Aeniur"));
+        siswaList.add(new Siswa("Vyennicht", "Aeniur"));
+        siswaList.add(new Siswa("Reinorareste", "Aeniur"));
+        siswaList.add(new Siswa("Seresya", "Aeniur"));
+        siswaList.add(new Siswa("Ayseres", "Aeniur"));
+        siswaList.add(new Siswa("Haronel", "Aeniur"));
+        siswaList.add(new Siswa("Arenvin", "Aeniur"));
+        siswaList.add(new Siswa("Lawrence", "Aeniur"));
+        siswaList.add(new Siswa("Icerta", "Aeniur"));
+        siswaList.add(new Siswa("Riferell", "Aeniur"));
+        siswaList.add(new Siswa("Vyennicht", "Aeniur"));
+        siswaList.add(new Siswa("Reinorareste", "Aeniur"));
+
+        adapter = new SiswaAdapter(this, siswaList);
+        recyclerView.setAdapter(adapter);
     }
 }
