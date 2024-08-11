db835ff9742d07e383376ffea3c702c922a3cb5d
SafiaRashidaRaya <140325055+SafiaRashidaRaya@users.noreply.github.com>
2024-08-11 13:23:16 +0700

    versi 3.7

--- SQLiteDatabase/app/src/main/java/com/synx/sqlitedatabase/MainActivity.java
+++ SQLiteDatabase/app/src/main/java/com/synx/sqlitedatabase/MainActivity.java
@@ -54,8 +54,12 @@ public class MainActivity extends AppCompatActivity {
         }else {
             if (pilihan.equals("Insert")){
                 String sql = "INSERT INTO tblbarang (barang, stok, harga) VALUES ('"+barang+"', "+stok+", "+harga+")";
-                db.runSQL(sql);
-                pesan("Insert berhasil");
+                if (db.runSQL(sql)){
+                    pesan("Insert berhasil");
+                }else {
+                    pesan("Insert gagal");
+                }
+
             }else {
                 pesan("update");
             }
