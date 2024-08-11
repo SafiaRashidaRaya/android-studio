2b455b6847e7c35c29a845c8f3e1b5a8436d2937
SafiaRashidaRaya <140325055+SafiaRashidaRaya@users.noreply.github.com>
2024-08-11 15:26:39 +0700

    versi 3.9

--- SQLiteDatabase/app/src/main/java/com/synx/sqlitedatabase/BarangAdapter.java
+++ SQLiteDatabase/app/src/main/java/com/synx/sqlitedatabase/BarangAdapter.java
@@ -1,6 +1,7 @@
 package com.synx.sqlitedatabase;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -23,11 +24,15 @@ public class BarangAdapter extends RecyclerView.Adapter<BarangAdapter.ViewHolder
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_barang, parent, false);
+        return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.tvBarang.setText(barangList.get(position).getBarang());
+        holder.tvStok.setText(barangList.get(position).getStok());
+        holder.tvHarga.setText(barangList.get(position).getHarga());
 
     }
 
